870f0b8487548a8b5dee60a618d4738a808db3cc
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-12-10 00:16:47 -0500

    'from os.path import exists' in ext/safe_path.py

--- ranger/ext/safe_path.py
+++ ranger/ext/safe_path.py
@@ -2,21 +2,21 @@
 # License: GNU GPL version 3, see the file "AUTHORS" for details.
 
 from __future__ import absolute_import
-import os
+from os.path import exists
 
 SUFFIX = '_'
 
 
 def get_safe_path(dst):
-    if not os.path.exists(dst):
+    if not exists(dst):
         return dst
     if not dst.endswith(SUFFIX):
         dst += SUFFIX
-        if not os.path.exists(dst):
+        if not exists(dst):
             return dst
     n = 0
     test_dst = dst + str(n)
-    while os.path.exists(test_dst):
+    while exists(test_dst):
         n += 1
         test_dst = dst + str(n)
 
