8e94ad25cb89476fa7724279c4edb90c15b5509b
gigamicro <gammamicro@gmail.com>
2024-08-02 22:36:27 -0400

    716a2e5da2f60228e9d895b24d659fa51ecbc1ea re-revert

--- ranger/core/main.py
+++ ranger/core/main.py
@@ -250,19 +250,13 @@ https://github.com/ranger/ranger/issues
         # print the exit message if any
         if exit_msg:
             sys.stderr.write(exit_msg)
-
-    return exit_code  # pylint: disable=lost-exception
+        return exit_code  # pylint: disable=lost-exception
 
 
 def get_paths(args):
     if args.paths:
-        prefix = 'file://'
-        paths = []
-        for path in args.paths:
-            if path.startswith(prefix):
-                paths.append(urllib.parse.unquote(urllib.parse.urlparse(path).path))
-            else:
-                paths.append(path)
+        from urllib.parse import urlparse, unquote
+        paths = [unquote(urlparse(path).path) for path in args.paths]
     else:
         start_directory = os.environ.get('PWD')
         is_valid_start_directory = start_directory and os.path.exists(start_directory)
